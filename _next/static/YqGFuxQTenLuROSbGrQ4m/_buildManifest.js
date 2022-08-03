511965075161bc9baaa221b3bf2834d73d18cf8f
ayushsubedi <45836811+ayushsubedi@users.noreply.github.com>
2022-08-03 06:47:25 +0000

    Deploying to gh-pages from  @ 7f104c7d1776278d83ca3f3a9767b61728915a0d 🚀

---
+++ _next/static/YqGFuxQTenLuROSbGrQ4m/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/81b14a96ad212c09.css","static/chunks/pages/index-1a0a073022ab6b27.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
